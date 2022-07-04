6303f0d8219fd4179ea7f2447d2965f50fd29dfa
James Swent <jake@osprey21.com>
2022-07-04 12:58:14 -0400

    added plugins

---
+++ plugins/init.zsh
@@ -0,0 +1,8 @@
+# 
+# $ZSH/plugins/init.zsh 
+# 
+
+source $ZSH/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $ZSH/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+#source $ZSH/plugins/git.plugin.zsh
+source $ZSH/plugins/nvm.plugin.zsh
