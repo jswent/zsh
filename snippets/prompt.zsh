703f0d514d50a019e5cc5075e471348982b6a72b
jswent <94636565+jswent@users.noreply.github.com>
2022-07-08 16:19:10 -0400

    fixed exit code spacing

--- snippets/prompt.zsh
+++ snippets/prompt.zsh
@@ -4,7 +4,7 @@ function check_last_exit_code() {
   local LAST_EXIT_CODE=$?
   if [[ $LAST_EXIT_CODE -ne 0 ]]; then
     local EXIT_CODE_PROMPT=' '
-    EXIT_CODE_PROMPT+="%{$fg_bold[red]%}%{$reset_color%}"
+    EXIT_CODE_PROMPT+="%{$fg_bold[red]%} %{$reset_color%}"
     EXIT_CODE_PROMPT+="%{$fg_bold[red]%}$LAST_EXIT_CODE  %{$reset_color%}"
     echo "$EXIT_CODE_PROMPT"
   else 
