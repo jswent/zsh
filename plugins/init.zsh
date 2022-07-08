7376492a62072ca3754e9ac495f583713b7d2fc7
jswent <94636565+jswent@users.noreply.github.com>
2022-07-08 14:32:59 -0400

    added fzf completion

--- plugins/init.zsh
+++ plugins/init.zsh
@@ -6,3 +6,5 @@ source $ZSH/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source $ZSH/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 #source $ZSH/plugins/git.plugin.zsh
 source $ZSH/plugins/nvm.plugin.zsh
+source $ZSH/plugins/git-prompt.zsh
+source $ZSH/plugins/fzf/*.zsh
