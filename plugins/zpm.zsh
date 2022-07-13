868aef8b50fb852ad85a9f0780b0480e375fbb38
jswent <94636565+jswent@users.noreply.github.com>
2022-07-13 16:26:19 -0400

    added omz interoperability with plugin manager

--- plugins/zpm.zsh
+++ plugins/zpm.zsh
@@ -23,12 +23,38 @@ source_local_plugin () {
   fi
 }
 
+is_omz_plugin () {
+  local name=$1
+  local omz_plugins=$(<$ZSH/plugins/omz-plugins.txt)
+  for omz_plugin in $=omz_plugins; do 
+    if [ "$omz_plugin" = "$name" ]; then
+      return true
+    else 
+      return false
+    fi 
+  done
+}
+
+source_omz_plugin () {
+  local name=$1
+  if [ -n "$name" ]; then
+    print -P "%{$fg_bold[green]%}ZPM: Downloading $name plugin from oh-my-zsh...\n%{$reset_color%}"
+    curl "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/$name/$name.plugin.zsh" >> $ZSH/plugins/$name.plugin.zsh 
+    [ -f "$ZSH/plugins/$name.plugin.zsh" ] && source "$ZSH/plugins/$name.plugin.zsh"
+    print -P "\n%{$fg_bold[green]%}ZPM: $name successfully installed\n%{$reset_color%}"
+  fi 
+}
+
 if [ -n "$plugins" ]; then
-  for plugin in $=plugins; do 
+  for plugin in $=plugins; do
+    # Source config files before loading plugin
+    [ -f "$ZSH/config/$plugin.zsh" ] && source "$ZSH/config/$plugin.zsh"
+    
     if is_local_plugin "$plugin"; then
       source_local_plugin "$plugin"
+    elif is_omz_plugin "$plugin"; then
+      source_omz_plugin "$plugin"
     fi
 
-    [ -f "$ZSH/config/$plugin.zsh" ] && source "$ZSH/config/$plugin.zsh"
   done 
-fi      
+fi   
