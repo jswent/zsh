fde4698e1e337f05107fa1d6a2da75ccd706859b
James Swent <jake@osprey21.com>
2024-08-26 00:37:41 -0400

    feat(keymaps): Refactor `launchranger` to support multiple...
    TUI file managers.

--- keymaps.zsh
+++ keymaps.zsh
@@ -7,8 +7,18 @@ launchneovim () { nvim . }
 zle -N launchneovim
 
 launchranger () { ranger <$TTY; zle redisplay; }
-zle -N launchranger 
+launchfiles () { 
+  if command -v yazi >/dev/null 2>&1; then
+    yazi <$TTY
+  elif command -v ranger >/dev/null 2>&1; then
+    ranger <$TTY
+  else
+    echo "Error: No TUI file manager found."
+  fi
+  zle redisplay
+}
+zle -N launchfiles
 
 # Launch applications
-bindkey '^f' launchranger
+bindkey '^f' launchfiles
 bindkey '^v' launchneovim
