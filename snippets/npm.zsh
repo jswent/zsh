4378a31ab4b35bf2172eaa2e7330c698cfa0e107
James Swent <jake@osprey21.com>
2025-04-05 17:55:39 -0400

    feat(node): Override pkg mgr default paths

---
+++ snippets/npm.zsh
@@ -0,0 +1,12 @@
+# NPM
+export npm_config_cache="$HOME/.local/npm"
+
+# Yarn
+export YARN_CACHE_FOLDER="$HOME/.local/yarn"
+export YARN_RC_FILENAME="$HOME/.config/.yarnrc"
+
+# PNPM
+export PNPM_STORE_PATH="$HOME/.local/pnpm"
+
+# Bun
+export BUN_INSTALL="$HOME/.local/bun"
